cd75a5b54cd2fd3a8156cf9d315d494b68d97a30
Pranjal <pranjaldhar002@gmail.com>
2024-12-23 19:41:27 +0530

    Your commit message describing the changes

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
+
 const userRouter = require("./routes/userRouter");
 const connectdb = require("./db/connection");
 require("./models/usertable");
 
 const app = express();
-app.use(express.json()); // Middleware to parse JSON requests
-
+app.use(cors());
+app.use(express.json());
+//
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 const port = 8000;
 
-// Database connection
 const database =
     "mongodb+srv://pranjaldhar002:Alphadigama09@cluster0.45hsa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.set("strictQuery", false);
 connectdb(database);
 
-// Use userRouter for all routes starting with /
+
 app.use("/", userRouter);
 
 app.listen(port, () => {
